4ae2bb9325a1d93c8cc07f1436547da187de0f82
sank108 <137850649+sank108@users.noreply.github.com>
2024-12-15 00:55:19 +0530

    Exploring different HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,6 +2,31 @@ const express = require("express");
 
 const app = express();
 
+app.use("/users", (req, res) => {
+  res.send("Haahahha");
+});
+
+app.get("/users", (req, res) => {
+  res.send("This will only match the get method");
+});
+
+app.post("/users", (req, res) => {
+  res.send("Send data to the data base");
+});
+
+app.delete("/users", (req, res) => {
+  res.send("User has been successfully deleted");
+});
+
+//All the below routes will match all the HTTP method API calls
+// app.use("/", (req, res) => {
+//   res.send("Base Route");
+// });
+
+app.use("/home", (req, res) => {
+  res.send("Hello from home");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello from the server-1"); // this function is known as request handler
 });
