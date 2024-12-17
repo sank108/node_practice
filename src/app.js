3b88a8e9ed3909f4b4a6e1ad403c93fc187134ad
sank108 <137850649+sank108@users.noreply.github.com>
2024-12-17 23:31:17 +0530

    Multiple routes

--- src/app.js
+++ src/app.js
@@ -2,34 +2,46 @@ const express = require("express");
 
 const app = express();
 
-app.use("/users", (req, res) => {
-  res.send("Haahahha");
-});
+// app.use("/users", (req, res) => {
+//   res.send("Haahahha");
+// });
 
-app.get("/users", (req, res) => {
-  res.send("This will only match the get method");
-});
+// app.get("/users/:id", (req, res) => {
+//   console.log(req.params);
 
-app.post("/users", (req, res) => {
-  res.send("Send data to the data base");
-});
+//   res.send("This will only match the get method");
+// });
 
-app.delete("/users", (req, res) => {
-  res.send("User has been successfully deleted");
-});
+// app.post("/users", (req, res) => {
+//   res.send("Send data to the data base");
+// });
+
+// app.delete("/users", (req, res) => {
+//   res.send("User has been successfully deleted");
+// });
 
 //All the below routes will match all the HTTP method API calls
 // app.use("/", (req, res) => {
 //   res.send("Base Route");
 // });
 
-app.use("/home", (req, res) => {
-  res.send("Hello from home");
-});
+// app.use("/home", (req, res) => {
+//   res.send("Hello from home");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the server-1"); // this function is known as request handler
-});
+// app.use("/test", (req, res) => {
+//   res.send("Hello from the server-1"); // this function is known as request handler
+// });
+
+app.use(
+  "/user",
+  (req, res) => {
+    res.send("1st");
+  },
+  (req, res) => {
+    res.send("2nd");
+  }
+);
 
 app.listen(3000, (req, res) => {
   console.log("Server is successfully listening on port 3000");
